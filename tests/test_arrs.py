598ab6e76a60fe64d2cfed3a6a84c3670f1e2167
Ivan <ivan.koltsov.1994@mail.com>
2023-02-08 22:27:15 +0500

    100 процентное покрытие тестами

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,5 +7,12 @@ def test_get():
 
 
 def test_slice():
-    assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
-    assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    def test_slice():
+        assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
+        assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+        assert arrs.my_slice([1, 2, 3], 0) == [1, 2, 3]
+        assert arrs.my_slice([], ) == []
+        assert arrs.my_slice([1, 2, 3, 4], -5, 2) == [1, 2]
+        assert arrs.my_slice([1, 2, 3, 4], -3, 3) == [2, 3]
+
+# 100 процентное покрытие
